c60d2747ea4e6c0e59d2ebcfe73825a1673d6053
Kliazovich Alex <aliaksandrkliazovich@gamil.com>
2020-11-08 23:26:51 +0300

    Table of users

--- src/Text.js
+++ src/Text.js
@@ -1,33 +1,17 @@
 import React, { Component } from "react";
-
+import data from "./data.json";
 import "./index.css";
 
 class Text extends Component {
-  state = {
-    italic: false,
-  };
-
-  click = () => {
-    this.state.italic === false
-      ? this.setState((this.state = { italic: true }))
-      : this.setState((this.state = { italic: false }));
-    console.log(this.state.italic);
-  };
-
   render() {
-    return (
-      <div
-        className={this.state.italic === true ? "text-style" : ""}
-        onClick={this.click}
-      >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
-        ipsa enim mollitia exercitationem explicabo repellendus deleniti quidem
-        dolore, cum magni quod tempore voluptate unde laudantium molestias
-        excepturi rem velit reprehenderit ipsum nesciunt? Quae laborum quasi,
-        amet, illo, aliquam totam provident est dignissimos praesentium tempora
-        libero ex. Incidunt, placeat optio. Corrupti.
-      </div>
-    );
+    return data.map((element) => {
+      return (
+        <div className="user">
+          <h1>{element.name.first}</h1>
+          <h2>{element.name.last}</h2>
+        </div>
+      );
+    });
   }
 }
 
